2093ab7aa0ca84ed6f5ec5608f000c7595ed63e8
samuel obunge <samuelobunge@gmail.com>
2023-09-07 01:35:56 -0700

    change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -7,5 +7,5 @@
 int main(void)
 {
 	/* Use the puts function to print the string and add a new line */
-	puts("Programming is like building a multilingual puzzle");
+	puts(""Programming is like building a multilingual puzzle");
 	return (0); }
