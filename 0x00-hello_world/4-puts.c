58d0d9be31ac466b3204d382f65346a9c1c4a02a
samuel obunge <samuelobunge@gmail.com>
2023-09-07 01:25:29 -0700

    change

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// Use the puts function to print the string and add a new line
+/* Use the puts function to print the string and add a new line */
 int main(void)
 {
 	/* Use the puts function to print the string and add a new line */
