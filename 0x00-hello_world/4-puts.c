dbc1e58583c8543e348472d2bc563a509fd938d0
samuel obunge <samuelobunge@gmail.com>
2023-09-07 00:56:51 -0700

    4 puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Use the function puts to print the message
+    puts("Programming is like building a multilingual puzzle");
+
+    // Return 0 to indicate success
+    return (0);
+}
