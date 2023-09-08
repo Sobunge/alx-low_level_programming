ad9fb3be8cb782cf7377d264fd7d4144f2303921
samuel obunge <samuelobunge@gmail.com>
2023-09-08 05:59:11 -0700

    change

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Print all possible combinations of single-digit numbers */
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			if (i < j)
+			{
+				putchar(i + '0');
+				putchar(',');
+				putchar(' ');
+				putchar(j + '0');
+				putchar('\n');
+			}
+		}
+	}
+	return (0); }
