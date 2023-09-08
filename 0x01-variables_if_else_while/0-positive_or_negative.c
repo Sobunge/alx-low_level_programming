5bd32d7a8e30c51c680f9d35001f79df6e235e7c
samuel obunge <samuelobunge@gmail.com>
2023-09-08 04:03:47 -0700

    change

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* Check if 'n' is positive, negative, or zero, and print the result */
 	if (n > 0) {
-        	printf("%d is positive\n", n);
-    	} else if (n < 0) {
-        	printf("%d is negative\n", n);
-   	} else {
-        	printf("%d is zero\n", n);
-    	}
+		printf("%d is positive\n", n);
+	} else if (n < 0) {
+	printf("%d is negative\n", n);
+	} else {
+	printf("%d is zero\n", n);
+	}
 	return (0);
 }
