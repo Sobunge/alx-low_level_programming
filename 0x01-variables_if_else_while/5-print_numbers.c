d4cd57939c8628035bf60a400cbfc466e9f66183
samuel obunge <samuelobunge@gmail.com>
2023-09-08 05:20:14 -0700

    change

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		putchar('0' + i);
+		putchar('\n');
+	}
+	return (0); }
