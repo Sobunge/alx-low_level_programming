c84add03eaf01c4a004ad158b65ea17540dfbf70
samuel obunge <samuelobunge@gmail.com>
2023-09-08 05:11:50 -0700

    change

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+			{
+				putchar(c);
+			}
+	}
+    
+	putchar('\n');
+	return (0); }
