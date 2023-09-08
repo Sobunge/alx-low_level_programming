a10a3947879589086fa7cdcfb82b5a3545815ffd
samuel obunge <samuelobunge@gmail.com>
2023-09-08 05:31:29 -0700

    change

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/* Print single-digit numbers from 0 to 9 using 'putchar' */
+	char digit = '0'; /* Initialize with the character '0' */
+
+	while (digit <= '9')
+	{
+		putchar(digit);
+		digit++;
+	}
+	putchar('\n'); /* Print a newline character */
+	return (0); }
