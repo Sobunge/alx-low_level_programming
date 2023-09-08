b51b8baa7f862143ca9b1a6ce70b5e9905751240
samuel obunge <samuelobunge@gmail.com>
2023-09-08 05:45:54 -0700

    change

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 /* Print the lowercase alphabet in reverse using 'putchar' */
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n'); /* Print a newline character */
+	return (0); }
