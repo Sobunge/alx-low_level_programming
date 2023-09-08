a3ae19fb116d41da99f615f0d7460acb2e42cf6d
samuel obunge <samuelobunge@gmail.com>
2023-09-08 05:52:54 -0700

    change

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	 /* Print hexadecimal digits in lowercase using 'putchar' */
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n'); /* Print a newline character */
+	return (0); }
