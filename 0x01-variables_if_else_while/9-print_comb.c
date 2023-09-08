11319fb1ba6c4b97ccafaf7a33d1eddc8227dfc9
samuel obunge <samuelobunge@gmail.com>
2023-09-08 06:12:28 -0700

    change

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,9 +9,9 @@ int main(void)
 	/* Print all possible combinations of single-digit numbers */
 	int i, j;
 
-	for (i = 0; i <= 9; i++)
+	for (i = 1; i <= 9; i++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (j = i +  1; j <= 9; j++)
 		{
 			if (i < j)
 			{
