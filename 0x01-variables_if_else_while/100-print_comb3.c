a0587c40f5503d95581e2bfbba07eb686e02a453
samuel obunge <samuelobunge@gmail.com>
2023-09-08 07:44:35 -0700

    change

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= 8; i++)
+	{
+		for (int j = i + 1; j <= 9; j++)
+		{
+		putchar(i + '0'); // Print the first digit
+		putchar(j + '0'); // Print the second digit
+			if (i != 8 || j != 9)
+			{
+				putchar(','); /* Print a comma */
+				putchar(' '); /* Print a space */
+			}
+		}
+	}
+	putchar('\n'); /* Print a newline character */
+	return (0); }
