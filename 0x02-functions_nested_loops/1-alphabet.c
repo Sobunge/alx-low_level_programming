afe9c8c7031210730694a48317367265c4b3350f
samuel obunge <samuelobunge@gmail.com>
2023-09-12 06:04:29 -0700

    change

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,10 @@
 #include "main.h"
 
+/**
+ * function -Print alphabet
+ * Description: Use thr puts function to print the string and add a new line
+ * Return: Always 0 (Success)
+ */
 void print_alphabet(void)
 {
 	char letter = 'a';
