c650e6c0a2786bebf6f435af066cd22b476a7c8a
samuel obunge <samuelobunge@gmail.com>
2023-09-12 08:03:39 -0700

    change

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include "main.h"
 
+/**
+ * jack_bauer -  function that prints every minute of the day of Jack Bauer
+ * starting from 00:00 to 23:59.
+ * Return: nothing.
+ */
 void jack_bauer(void)
 {
 	for (int hour = 0; hour < 24; hour++)
@@ -10,13 +16,3 @@ void jack_bauer(void)
 		}
 	}
 }
-
-/**
- * main - Entry point
- * Description: Use thr puts function to print the string and add a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	jack_bauer();
-	return (0); }
