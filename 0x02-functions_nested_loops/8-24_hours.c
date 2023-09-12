e41e0cdd69370c00d7ebcb1cb53aab0b036575b6
samuel obunge <samuelobunge@gmail.com>
2023-09-12 08:08:10 -0700

    change

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,9 +8,11 @@
  */
 void jack_bauer(void)
 {
-	for (int hour = 0; hour < 24; hour++)
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
 	{ /* Hours range from 0 to 23 */
-		for (int minute = 0; minute < 60; minute++)
+		for (minute = 0; minute < 60; minute++)
 		{ /* Minutes range from 0 to 59 */
 			printf("%02d:%02d\n", hour, minute); /* Print the time in HH:MM format */
 		}
