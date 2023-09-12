69011579c5ad7710e2021d1ee880c2b5225eb424
samuel obunge <samuelobunge@gmail.com>
2023-09-12 08:14:46 -0700

    change

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @a: input integer 1
+ * @b: input integer 2
+ * Return: int.
+ */
 
 int add(int a, int b) 
 {
 	int result = a + b;
-	return result;
+	return (result);
 }
-
-/**
- * main - Entry point
- * Description: Use thr puts function to print the string and add a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int n;
-
-	n = add(89, 9);
-	printf("%d\n", n);	
-	return (0); }
