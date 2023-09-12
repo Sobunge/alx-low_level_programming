3b459bb34c1c7264953ce6fe6f430f745da8b5c7
samuel obunge <samuelobunge@gmail.com>
2023-09-12 07:31:28 -0700

    change

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,20 +9,19 @@
   */
 int print_sign(int n)
 {
-	int sign;
-
 	if (n > 0)
 	{
-		putchar('+');
-		sign = 1;
-	} else if (n == 0)
-	{
-		putchar('0');
-		sign = 0;
-	} else
-	{
-		putchar('-');
-		sign = -1;
+		_putchar(43);
+		return (1);
 	}
-	return sign;
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
 }
