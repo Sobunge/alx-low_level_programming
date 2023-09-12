ccf7d59e0fa1f43a99fa926098ac11c028a9b49c
samuel obunge <samuelobunge@gmail.com>
2023-09-12 07:01:50 -0700

    change

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,50 +1,23 @@
 #include <stdio.h>
 
+/**
+  * print_sign - Prints the sign of a number
+  * @n: The number to be checked
+  *
+  * Return: 1 for positive num, -1 for negative num or zero for anything else
+  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		printf("+");
-		return 1;
+		return (1);
 	} else
 		if (n == 0)
 		{
 			printf("0");
-			return 0;
-		} else
-		{
-			printf("-");
-			return -1;
-		}
+			return (0);
+		} 
+		printf("-");
+		return (-1);
 }
-
-/**
- * main - Entry point
- * Description: Use thr puts function to print the string and add a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int r;
-
-	r = print_sign(98);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(0xff);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	r = print_sign(-1);
-	_putchar(',');
-	_putchar(' ');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0); }
