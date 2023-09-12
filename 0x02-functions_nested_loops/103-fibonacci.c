7c7af77f882af2c15d31b29315f4286274e950bc
samuel obunge <samuelobunge@gmail.com>
2023-09-12 09:09:57 -0700

    change

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,12 +15,12 @@ int main(void)
 	{
 		if (current % 2 == 0)
 		{
-			sum += current; /* Add even Fibonacci number to the sum */
+			sum += current; /* Add even Fibonacci to the sum */
 		}
 	/* Calculate the next Fibonacci number */
 	next = prev + current;
 	prev = current;
 	current = next;
 	}
-	printf("The sum of even-valued Fibonacci terms not exceeding 4,000,000 is: %ld\n", sum);
+	printf("%ld\n", sum);
 	return (0); }
