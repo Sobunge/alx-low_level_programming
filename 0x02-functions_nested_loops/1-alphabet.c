a9a6f84f9334cce21116726d0095057b204f43f9
samuel obunge <samuelobunge@gmail.com>
2023-09-12 05:55:43 -0700

    change

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include "main.h"
 
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+}
+
 /**
  * main - Entry point
  * Description: Use thr puts function to print the string and add a new line
@@ -8,12 +20,5 @@
  */
 int main(void)
 {
-	char letter = 'a';
-
-	while (letter <= 'z')
-	{
-	_putchar(letter);
-	letter++;
-	}
-	_putchar('\n');
+	print_alphabet();
 	return (0); }
