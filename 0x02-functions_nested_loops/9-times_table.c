c5a8dd8f68774bb522e2e648c0831e0ccb66daa4
samuel obunge <samuelobunge@gmail.com>
2023-09-12 08:11:19 -0700

    change

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,10 +7,32 @@
  */
 void times_table(void)
 {
-	int i;
+	int row, col;
+	int mult = 0;
 
-	for (i = 0; i <= 10; i++)
+	for (row = 0; row < 10; row++)
 	{
-		printf("9 x %d = %d\n", i, 9 * i);
+		for (col = 0; col < 10; col++)
+		{
+			mult = (row * col);
+			if (mult < 10)
+			{
+				if (col != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+				}
+				_putchar(mult + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar((mult / 10) + '0');
+				_putchar((mult % 10) + '0');
+			}
+			if (col != 9)
+				_putchar(',');
+		}
+		_putchar('\n');
 	}
 }
