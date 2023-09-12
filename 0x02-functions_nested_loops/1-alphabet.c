4093ff417ad329e6201d36c8be7197b009b7a39a
samuel obunge <samuelobunge@gmail.com>
2023-09-12 06:20:55 -0700

    change

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,8 +6,6 @@
  * Return: void (Success)
  *
  * struct print_alphabet - Prints the lowercase alphabet followed by a newline.
- *
- * Description: This function prints the lowercase alphabet from 'a' to 'z'
  * followed by a newline character '\n'. It uses the putchar function from
  * the standard library to print each character.
  */
