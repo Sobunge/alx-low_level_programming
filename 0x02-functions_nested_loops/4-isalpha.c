0d6b1c8b63893ece0fc856cac3a7d7e46bcc2f7d
samuel obunge <samuelobunge@gmail.com>
2023-09-12 06:59:01 -0700

    change

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,32 +1,16 @@
 #include <stdio.h>
 
+/**
+  * _isalpha - Checks for alphabetic character
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic character or 0 for anything else
+  */
 int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
-		return 1; /* The character is alphabetic */
-	} else
-	{
-		return 0; /* The character is not alphabetic */
+		return (1); /* The character is alphabetic */
 	}
+	return (0); /* The character is not alphabetic */
 }
-
-/**
- * main - Entry point
- * Description: Use thr puts function to print the string and add a new line
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	int r;
-
-	r = _isalpha('H');
-	_putchar(r + '0');
-	r = _isalpha('o');
-	_putchar(r + '0');
-	r = _isalpha(108);
-	_putchar(r + '0');
-	r = _isalpha(';');
-	_putchar(r + '0');
-	_putchar('\n');
-	return (0); }
