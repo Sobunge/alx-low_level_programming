b083746c3affc553ef8c70f6ff212fe55647292c
samuel obunge <samuelobunge@gmail.com>
2023-09-12 08:43:05 -0700

    cange

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,12 +9,13 @@ int main(void)
 	long long int fib[50]; /* Store Fibonacci numbers as long long integers to handle large values */
 	fib[0] = 1;
 	fib[1] = 2;
+	int i;
 
-	for (int i = 2; i < 50; i++)
+	for (i = 2; i < 50; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 	}
-	for (int i = 0; i < 50; i++)
+	for (i = 0; i < 50; i++)
 	{
 		printf("%lld", fib[i]);
 		if (i < 49)
