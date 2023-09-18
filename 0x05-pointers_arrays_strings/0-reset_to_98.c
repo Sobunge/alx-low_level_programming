14ce9db2dbe9000b514f45d3d4e4c4e06ac65e9f
samuel obunge <samuelobunge@gmail.com>
2023-09-17 22:25:23 -0700

    adding comment update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
-// Author By Samuel Obunge
-// URL = https://github.com/Sobunge
+/* Author By Samuel Obunge */
+/* URL = https://github.com/Sobunge  *//
 /**
  
  * @n: An integer input pointer
