0661393c7d47d5adf3ab876472dd2c6b32c13c96
samuel obunge <samuelobunge@gmail.com>
2023-09-18 12:46:19 -0700

    change format of 4-print_rev file

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,22 +1,20 @@
-# include "main.h"
+#include "main.h"
 
-  /** 
+/**
    * print_rev- print reversed string, followed by a new line
-   * @s: pointer to the string to point 
+   * @s: pointer to the string to point
    * Return: void
-   */
+*/
 
-   void _print_rev(char *s) 
-  {
-     int swap;
+void _print_rev(char *s)
+{
+	int i = 0;
 
-     int i = 0;
-     while (s[i]) 
+	while (s[i])
             i++;
-  while (i--) 
-{  
-      _putchar(s[i]);
-  } 
-     _putchar('/n');
-
-  } 
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('/n');
+}
