aa9a1c48d9e27d7a9510abe30e3221e51533d251
samuel obunge <samuelobunge@gmail.com>
2023-09-17 22:29:16 -0700

    change

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /* Author By Samuel Obunge */
 /* URL = https://github.com/Sobunge  */
-/**
- 
- * @n: An integer input pointer
- */
+/** 
+* @n: An integer input pointer
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
