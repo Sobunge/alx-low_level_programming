241ca77336142bdf9f18d46972c6befa15082bfb
samuel obunge <samuelobunge@gmail.com>
2023-09-25 12:40:12 -0700

    change

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include <stddef.h>
+#include "main.h"
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *original_dest = dest; /* Save the original destination pointer */
+
+	while (n > 0)
+	{
+		*dest = *src; /* Copy a byte from src to dest */
+		dest++; /* Move to the next destination byte */
+		src++; /* Move to the next source byte */
+		n--; /* Decrease the remaining count */
+	}
+
+	return (original_dest); /* Return the original destination pointer */
+}
