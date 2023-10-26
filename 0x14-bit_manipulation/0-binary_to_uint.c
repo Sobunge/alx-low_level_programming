91b8a5c851bd95cac3b7fc7b9bca84f7ace7a978
samuel obunge <samuelobunge@gmail.com>
2023-10-26 08:56:21 -0700

    change

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result = 0;
+
+	if (b == NULL)
+		return (0);
+
+	for (int i = 0; b[i] != '\0'; i++)
+	{
+		char c = b[i];
+		
+		if (c != '0' && c != '1')
+			{
+				return (0);
+				result = (result << 1) + (c - '0');
+			}
+
+		return (result);
+	}
+}
