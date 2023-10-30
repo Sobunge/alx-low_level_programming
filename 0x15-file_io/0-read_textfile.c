e9bb17ae7d31e7696aca8b22afacf7daa07744d6
samuel obunge <samuelobunge@gmail.com>
2023-10-30 04:41:36 -0700

    change

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,44 +10,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd; /* file descriptor */
-	char *buffer; /* buffer to store the file content */
-	ssize_t bytes_read, bytes_written; /* number of bytes 
-					      read and written */
+	ssize_t o, r, w;
+	char *buffer;
 
-	/* check if filename is NULL */
 	if (filename == NULL)
 		return (0);
-	/* open the file in read-only mode */
-	fd = open(filename, O_RDONLY);
 
-	/* check if the file can not be opened */
-	if (fd == -1)
-		return (0);
-	/* allocate memory for the buffer */
 	buffer = malloc(sizeof(char) * letters);
-
-	/* check if memory allocation failed */
 	if (buffer == NULL)
 		return (0);
-	/* read the file content into the buffer */
-	bytes_read = read(fd, buffer, letters);
 
-	/* check if the file can not be read */
-	if (bytes_read == -1)
-		return (0);
-	/* close the file */
-	close(fd);
-	/* write the buffer content to the standard output */
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
 
-	/* check if write failed or did not 
-	 * write the expected amount of bytes */
-	if (bytes_written == -1 || bytes_written != bytes_read)
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
 		return (0);
-	/* free the buffer */
+	}
+
 	free(buffer);
-	/* return the actual number of letters read 
-	 * and printed */
-	return (bytes_written);
+	close(o);
+
+	return (w);
 }
