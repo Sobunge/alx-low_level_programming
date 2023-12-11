d6d74765bce7c4ee1af89cfa37b0e1966b84563b
samuel obunge <samuelobunge@gmail.com>
2023-12-11 07:41:17 -0800

    change

--- 0x18-dynamic_libraries/example_library.c
+++ 0x18-dynamic_libraries/example_library.c
@@ -1,13 +1,39 @@
-// example_library.c
+// 100-operations.c
 
 #include <stdio.h>
 
-// Simple function that adds two integers
+// C function for addition
 int add(int a, int b) {
     return a + b;
 }
 
-// Function that prints a message
-void print_message(const char *message) {
-    printf("%s\n", message);
+// C function for subtraction
+int sub(int a, int b) {
+    return a - b;
 }
+
+// C function for multiplication
+int mul(int a, int b) {
+    return a * b;
+}
+
+// C function for division
+int div(int a, int b) {
+    if (b != 0) {
+        return a / b;
+    } else {
+        printf("Error: Division by zero\n");
+        return 0; // Handle division by zero gracefully
+    }
+}
+
+// C function for modulo
+int mod(int a, int b) {
+    if (b != 0) {
+        return a % b;
+    } else {
+        printf("Error: Modulo by zero\n");
+        return 0; // Handle modulo by zero gracefully
+    }
+}
+
