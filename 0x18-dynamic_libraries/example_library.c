3c756f4b08912af06c31fdf1f47abd9747b5c899
samuel obunge <samuelobunge@gmail.com>
2023-12-11 07:37:56 -0800

    change

---
+++ 0x18-dynamic_libraries/example_library.c
@@ -0,0 +1,13 @@
+// example_library.c
+
+#include <stdio.h>
+
+// Simple function that adds two integers
+int add(int a, int b) {
+    return a + b;
+}
+
+// Function that prints a message
+void print_message(const char *message) {
+    printf("%s\n", message);
+}
