c985345510c00fb0d52fcd798a99f63e9d8d29a1
Samuel Obunge <samuelobunge@yahoo.com>
2024-04-09 10:43:57 +0300

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -1,29 +1,52 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "search_algos.h"
 
-int interpolation_search(int *array, size_t size, int value) {
-    if (array == NULL || size == 0) {
-        return -1;
-    }
+/**
+ * interpolation_search - searches for a value in an array of
+ * integers using the Interpolation search algorithm
+ *
+ * @array: input array
+ * @size: size of the array
+ * @value: value to search in
+ * Return: index of the number
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+	size_t pos, low, high;
+	double f;
 
-    size_t low = 0;
-    size_t high = size - 1;
-    size_t pos;
+	if (array == NULL)
+		return (-1);
 
-    while (low <= high && value >= array[low] && value <= array[high]) {
-        pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
-        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+	low = 0;
+	high = size - 1;
 
-        if (array[pos] == value) {
-            return pos;  // Value found
-        } else if (array[pos] < value) {
-            low = pos + 1;  // Search in the right half
-        } else {
-            high = pos - 1;  // Search in the left half
-        }
-    }
+	while (size)
+	{
+		f = (double)(high - low) / (array[high] - array[low]) * (value - array[low]);
+		pos = (size_t)(low + f);
+		printf("Value checked array[%d]", (int)pos);
 
-    printf("Value checked array[%lu] is out of range\n", pos);
-    return -1;  // Value not found
+		if (pos >= size)
+		{
+			printf(" is out of range\n");
+			break;
+		}
+		else
+		{
+			printf(" = [%d]\n", array[pos]);
+		}
+
+		if (array[pos] == value)
+			return ((int)pos);
+
+		if (array[pos] < value)
+			low = pos + 1;
+		else
+			high = pos - 1;
+
+		if (low == high)
+			break;
+	}
+
+	return (-1);
 }
-
