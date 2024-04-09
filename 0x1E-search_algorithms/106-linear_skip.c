61cd621fb6eb16ea8e2027399ed90df2cf2eb804
Samuel Obunge <samuelobunge@yahoo.com>
2024-04-09 10:47:00 +0300

    Update 106-linear_skip.c

--- 0x1E-search_algorithms/106-linear_skip.c
+++ 0x1E-search_algorithms/106-linear_skip.c
@@ -1,41 +1,45 @@
-#include <stdio.h>
-#include <math.h>
 #include "search_algos.h"
 
 /**
- * linear_skip - Searches for a value in a sorted skip list of integers
- * @list: Pointer to the head of the skip list to search in
- * @value: Value to search for
+ * linear_skip - searches for a value in a skip list
  *
- * Return: A pointer to the first node where value is located,
- *         or NULL if value is not present in list or if head is NULL
+ * @list: input list
+ * @value: value to search in
+ * Return: index of the number
  */
 skiplist_t *linear_skip(skiplist_t *list, int value)
 {
-    if (list == NULL)
-        return (NULL);
+	skiplist_t *go;
 
-    skiplist_t *express = list->express, *prev = list;
+	if (list == NULL)
+		return (NULL);
 
-    while (express != NULL && express->n < value)
-    {
-        prev = express;
-        express = express->express;
+	go = list;
 
-        printf("Value checked at index [%lu] = [%d]\n", express->index, express->n);
-    }
+	do {
+		list = go;
+		go = go->express;
+		printf("Value checked at index ");
+		printf("[%d] = [%d]\n", (int)go->index, go->n);
+	} while (go->express && go->n < value);
 
-    printf("Value found between indexes [%lu] and [%lu]\n", prev->index, express->index);
+	if (go->express == NULL)
+	{
+		list = go;
+		while (go->next)
+			go = go->next;
+	}
 
-    skiplist_t *current = prev;
-    while (current != NULL && current->index <= express->index)
-    {
-        printf("Value checked at index [%lu] = [%d]\n", current->index, current->n);
-        if (current->n == value)
-            return (current);
-        current = current->next;
-    }
+	printf("Value found between indexes ");
+	printf("[%d] and [%d]\n", (int)list->index, (int)go->index);
 
-    return (NULL);
+	while (list != go->next)
+	{
+		printf("Value checked at index [%d] = [%d]\n", (int)list->index, list->n);
+		if (list->n == value)
+			return (list);
+		list = list->next;
+	}
+
+	return (NULL);
 }
-
